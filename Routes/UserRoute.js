720398f2db16fbcc8065a054822ee907d98bf820
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-03-04 11:21:09 +0530

    Forgot password endpoint created

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,4 +1,4 @@
-const { login, register, verifyUser, sendOTP } = require("../Controller/AuthController");
+const { login, register, verifyUser, sendOTP, forgotPasswordUserVerify, forgotPassword } = require("../Controller/AuthController");
 
 const router = require("express").Router();
 
@@ -6,5 +6,7 @@ router.route("/login").post(login);
 router.route("/register").post(register);
 router.route("/verifyUser/:userId").post(verifyUser);
 router.route("/sendOTP/:userId").get(sendOTP);
+router.route("/forgotPasswordUserVerify").post(forgotPasswordUserVerify);
+router.route("forgotPassword/:userId").post(forgotPassword);
 
 module.exports = router;
