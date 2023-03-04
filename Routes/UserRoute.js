c9cb06a3648efee3fc79b76fefd4286f49b47781
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-03-04 11:30:57 +0530

    update on forgot password

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -7,6 +7,6 @@ router.route("/register").post(register);
 router.route("/verifyUser/:userId").post(verifyUser);
 router.route("/sendOTP/:userId").get(sendOTP);
 router.route("/forgotPasswordUserVerify").post(forgotPasswordUserVerify);
-router.route("forgotPassword/:userId").post(forgotPassword);
+router.route("/forgotPassword/:userId").post(forgotPassword);
 
 module.exports = router;
